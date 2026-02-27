7f2416cdbd4ff94155acb693370b63f5d3c02e40
EricEduardo-prog <ericeduardo.prog@gmail.com>
2026-02-27 09:03:08 -0300

    Agrege .gitignore Quite node_modules del repo

---
+++ routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('layout',
+    {
+      title: 'E-E: Todo para su hogar o negocio',
+      pageCss: 'index',
+      currentPage: 'index',
+      body: 'pages/index'
+    });
+});
+
+/* GET login page */
+router.get('/login', function (req, res, next) {
+  res.render('layout',
+    {
+      title: 'Login - E-E',
+      pageCss: 'login_register',
+      currentPage: 'login',
+      body: 'pages/login'
+    });
+});
+
+/* GET register page */
+router.get('/register', function (req, res, next) {
+  res.render('layout',
+    {
+      title: 'Registrate - E-E',
+      pageCss: 'login_register',
+      currentPage: 'register',
+      body: 'pages/register'
+    });
+});
+
+/* GET cart page */
+router.get('/cart', function (req, res, next) {
+  res.render('layout',
+    {
+      title: 'Carrito - E-E',
+      pageCss: 'cart',
+      currentPage: 'cart',
+      body: 'pages/cart'
+    });
+});
+
+/* GET productDetail page */
+router.get('/productDetail', function (req, res, next) {
+  res.render('layout',
+    {
+      title: 'Detalle de producto - E-E',
+      pageCss: 'product',
+      currentPage: 'product',
+      body: 'pages/productDetail'
+    });
+});
+
+module.exports = router;
